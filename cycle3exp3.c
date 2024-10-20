3bb3951a6a4c2e6c1b56c35d7bb8d9ba6908c4eb
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2024-10-20 12:24:53 +0530

    Update cycle3exp3.c

--- cycle3exp3.c
+++ cycle3exp3.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-char postfix[100];  
-int postfixIndex = 0;  
-
-int isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/');
-}
+#include <ctype.h>
 
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
@@ -15,49 +9,62 @@ int precedence(char op) {
     return 0;
 }
 
-void infixToPostfix(char* expr, int start, int end) {
-    if (start == end) { 
-        postfix[postfixIndex++] = expr[start];
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+void infixToPostfixRec(char* infix, int start, int end, char* postfix, int* pIndex) {
+
+    if (start == end) {
+        postfix[(*pIndex)++] = infix[start];
         return;
     }
 
-    int minPrec = 100;
-    int mainOpPos = -1; 
-    int openBrackets = 0; 
-   
+    int minPrecedence = 4; 
+    int operatorIndex = -1; 
+    int parentheses = 0;
+
     for (int i = start; i <= end; i++) {
-        if (expr[i] == '(') {
-            openBrackets++;
-        } else if (expr[i] == ')') {
-            openBrackets--;
-        } else if (openBrackets == 0 && isOperator(expr[i]) && precedence(expr[i]) <= minPrec) {
-            minPrec = precedence(expr[i]);
-            mainOpPos = i;
+        if (infix[i] == '(') parentheses++;
+        else if (infix[i] == ')') parentheses--;
+        else if (isOperator(infix[i]) && parentheses == 0) {
+            int prec = precedence(infix[i]);
+            if (prec <= minPrecedence) {
+                minPrecedence = prec;
+                operatorIndex = i;
+            }
         }
     }
 
-    if (mainOpPos == -1) {
-        if (expr[start] == '(' && expr[end] == ')') {
-            infixToPostfix(expr, start + 1, end - 1); 
-        }
+    if (infix[start] == '(' && infix[end] == ')') {
+        infixToPostfixRec(infix, start + 1, end - 1, postfix, pIndex);
         return;
     }
 
-    infixToPostfix(expr, start, mainOpPos - 1);
-    infixToPostfix(expr, mainOpPos + 1, end);
-    postfix[postfixIndex++] = expr[mainOpPos];
+    infixToPostfixRec(infix, start, operatorIndex - 1, postfix, pIndex);
+
+    infixToPostfixRec(infix, operatorIndex + 1, end, postfix, pIndex);
+
+    postfix[(*pIndex)++] = infix[operatorIndex];
+}
+
+void infixToPostfix(char* infix, char* postfix) {
+    int n = strlen(infix);
+    int pIndex = 0;  
+
+    infixToPostfixRec(infix, 0, n - 1, postfix, &pIndex);
+
+    postfix[pIndex] = '\0';
 }
 
 int main() {
-    char expr[100];  
+    char infix[] = "1+(2/3)^3";
+    char postfix[100];  
 
-    printf("Enter an infix expression (with or without parentheses): ");
-    scanf("%s", expr);
+    infixToPostfix(infix, postfix);
 
-    int n = strlen(expr); 
-    infixToPostfix(expr, 0, n - 1);
-    postfix[postfixIndex] = '\0';
+    printf("Infix Expression: %s\n", infix);
+    printf("Postfix Expression: %s\n", postfix);
 
-    printf("Postfix expression: %s\n", postfix);
     return 0;
 }
