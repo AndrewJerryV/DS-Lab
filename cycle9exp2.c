9cdc34cc8a8fa8d25b764ba5d28abde0e7130ef5
Andrew Jerry V <142659281+AndrewJerryV@users.noreply.github.com>
2024-11-01 16:36:39 +0530

    Update and rename cycle9.c to cycle9exp2.c

--- cycle9exp2.c
+++ cycle9exp2.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-#define TABLE_SIZE 10
-
-int hashTable[TABLE_SIZE];
+int size;
+int hashTable[100];
 
 void insert(int key) {
-    int index = key % TABLE_SIZE;
+    int index = key % size;
     while (hashTable[index] != -1) 
-        index = (index + 1) % TABLE_SIZE;  
+        index = (index + 1) % size;  
     hashTable[index] = key;
 }
 
 void display() {
     printf("Hash Table:\n");
-    for (int i = 0; i < TABLE_SIZE; i++) {
+    for (int i = 0; i < size; i++) {
         if (hashTable[i] == -1) 
             printf("[%d] :\n", i);
         else 
@@ -22,12 +21,12 @@ void display() {
 }
 
 // int search(int key) {
-//     int index = key % TABLE_SIZE;
+//     int index = key % size;
 //     int startIndex = index;
 //     while (hashTable[index] != -1) {
 //         if (hashTable[index] == key) 
 //             return index;  
-//         index = (index + 1) % TABLE_SIZE;
+//         index = (index + 1) % size;
 //         if (index == startIndex) 
 //             break;
 //     }
@@ -36,22 +35,22 @@ void display() {
 
 int main() {
     int choice, key;
-
-    for (int i = 0; i < TABLE_SIZE; i++) {
+    printf("Enter size of table: ");
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++) {
         hashTable[i] = -1;
     }
 
-    printf("1.Insert key\n2.Display\n0.Exit\n");
+    printf("1.Insert key\n2.Display\n0.Exit");
     while (1) {
         printf("\nEnter choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter the key to insert: ");
+                printf("Enter key to insert: ");
                 scanf("%d", &key);
                 insert(key);
-                printf("Key %d inserted.\n", key);
                 break;
             case 2:
                 display();
